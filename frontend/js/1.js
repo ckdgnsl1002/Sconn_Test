aca6214fc3c0de0fd4132fab2561a3c6a6601fbc
21people <dnjsaud0427@naver.com>
2021-10-13 14:16:16 +0900

    Testing

--- frontend/js/1.js
+++
@@ -1,9 +0,0 @@
-const ITEM_KEY = "ITEM";
-const ITEMNO_KEY = "ITEMNO";
-const TELNO_KEY = "TELNO";
-const PW_KEY = "PW";
-
-localStorage.removeItem(ITEM_KEY);
-localStorage.removeItem(ITEMNO_KEY);
-localStorage.removeItem(TELNO_KEY);
-localStorage.removeItem(PW_KEY);
--- frontend/js/back.js
+++
@@ -1,8 +0,0 @@
-//뒤로가기
-const back = document.querySelector("#back");
-
-function goback() {
-  window.history.back();
-}
-
-back.addEventListener("click", goback);
